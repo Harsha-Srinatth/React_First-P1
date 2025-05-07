49d031e179380c9f82f24446a576012cce81f835
Harsha-Srinatth <harhsasrinatth5599@gmail.com>
2025-05-07 18:11:24 +0530

    updated images

--- src/context/Home.jsx
+++ src/context/Home.jsx
@@ -42,7 +42,7 @@ import Comments from './Comments'
       {/* Post Image */}
       <img
         className="w-full h-96 object-cover rounded-lg"
-        src= { `https://backend-folder-hdib.onrender.com/${post.image}` }
+        src= { `https://backend-folder-hdib.onrender.com/uploads/${post.image}` }
         alt="Post"
         width={96}
         height={77}
