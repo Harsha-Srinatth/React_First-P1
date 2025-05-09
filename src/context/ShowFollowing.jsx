47ffaf859b51812347a854fdeba25613dc30d012
Harsha-Srinatth <harhsasrinatth5599@gmail.com>
2025-05-09 11:55:19 +0530

    after

--- src/context/ShowFollowing.jsx
+++ src/context/ShowFollowing.jsx
@@ -45,32 +45,47 @@ const ShowFollowing = () => {
            
         };
         fetchData();
+        console.log(following);
     },[userId, navigate]);
 
     return(
-        <div>
-            <div className='text-white'>
-               <h1 className='text-white font-semibold'>
-                    Following List..!
-                </h1> 
-            
-            { following && following.length > 0 ? (following.map((user)=> {
+    <div className='text-white w-full' action="/explore">
+
+      <h1 className='flex flex-center font-bold text-3xl md:text-xl'>Users List</h1>
+      <div className='flex flex-col'>
+        {/* <input type='text' value={search} className='w-full p-2 mt-1' placeholder='Search For Others You Want To Know..?' onChange={(e) => setSearch(e.target.value)} /> */}
+        {following && following.length > 0 ? (
+          <div className='flex flex-start p-5'>
+            <div className='flex flex-col gap-4 '>
+              {following.map(user => (
                 <li key={user._id} >
-                <div className='flex flex-row gap-3 text-white'>
-                  <img src={ `https://backend-folder-hdib.onrender.com/uploads/${user.image}` } className='flex flex-row bg-yellow-600 rounded-full' alt='' width={56} height={56} />
-                  <div className='flex flex-col text-white gap-2'>
-                    <h1 className='text-lg text-2rem text-white font-semibold '>{user.username}</h1>
-                    <p className='text-sm text-white text-3rem'>{user.firstname}</p>
+                  <div className='flex flex-row gap-3'>
+                    <img src={ `https://backend-folder-hdib.onrender.com/uploads/${user.image}` } className='flex flex-row bg-yellow-600 rounded-full' alt='' width={56} height={56} />
+                    <div className='flex flex-col gap-2'>
+                      <h1 className='text-lg text-2rem font-semibold '>{user.username}</h1>
+                      <p className='text-sm text-3rem'>{user.firstname}</p>
+                    </div>
                   </div>
-                </div>
-              </li>
-            })) : (
-                <div>
-                    <p>No following user found</p>
-                </div>
-            )}
+                </li>
+              ))}
             </div>
-        </div>
+          </div>
+        ) : (
+          <div className='flex justify-center items-center h-screen'>
+            <div className='flex flex-col'>
+              <h1 className='text-yellow-600 font-semibold text-xl'>
+                No Following users
+              </h1>
+            </div>
+
+          </div>
+        )
+
+        }
+
+
+      </div>
+    </div>
     )
 };
 
